4fd0f69efa781c9298596a09a1746239ab6eb8fa
Julia <julia4eck@gmail.com>
2022-05-27 18:12:54 +0300

    Fixing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
 
-val sqlCreation = File("src\\main\\resources\\task1.sql").readText()
-val sqlInitialisation = File("src\\main\\resources\\task2_generated.sql").readText()
-val timesWithCashe = mutableListOf<Long>()
-val timesWithoutCashe = mutableListOf<Long>()
 const val coroutinesForSelections = 7000
 const val coroutinesAll = 10000
+val sqlCreation = File("src\\main\\resources\\task1.sql").readText()
+val sqlInitialisation = File("src\\main\\resources\\task2_generated.sql").readText()
+val timesWithCashe = MutableList(coroutinesAll * 2) { 0.toLong() }
+val timesWithoutCashe = MutableList(coroutinesAll * 2) { 0.toLong() }
 @Volatile
 var totalQueries = 0
 
@@ -22,16 +22,16 @@ private fun execute(useCashe: Boolean) {
     val proxy = Proxy()
     for (i in 0 until coroutinesForSelections) {
         GlobalScope.launch {
-            executeStatement(selectFromVisitors(), useCashe, proxy)
-            executeStatement(selectFromWaiters(), useCashe, proxy)
+            executeStatement(selectFromVisitors(), useCashe, proxy, i)
+            executeStatement(selectFromWaiters(), useCashe, proxy, i + 1)
             totalQueries += 2
         }
     }
     for (i in coroutinesForSelections until coroutinesAll) {
         GlobalScope.launch {
             val surname = if (i % 2 == 0) "Воронцова" else "Михайлова"
-            executeStatement(updateVisitors(surname), useCashe, proxy)
-            executeStatement(updateWaiters(8 + i % 2), useCashe, proxy)
+            executeStatement(updateVisitors(surname), useCashe, proxy, i)
+            executeStatement(updateWaiters(8 + i % 2), useCashe, proxy, i + 1)
             totalQueries += 2
         }
     }
@@ -43,13 +43,13 @@ private fun execute(useCashe: Boolean) {
     } / coroutinesAll} ns\n")
 }
 
-private fun executeStatement(sql: String, useCash: Boolean, proxy: Proxy): String {
+private fun executeStatement(sql: String, useCash: Boolean, proxy: Proxy, coroutineNum: Int): String {
     val start = System.nanoTime()
     val res = proxy.executeSQL(sql, useCash)
     if (useCash)
-        timesWithCashe.add(System.nanoTime() - start)
+        timesWithCashe[coroutineNum] = System.nanoTime() - start
     else
-        timesWithoutCashe.add(System.nanoTime() - start)
+        timesWithoutCashe[coroutineNum] = System.nanoTime() - start
     return res
 }
 
